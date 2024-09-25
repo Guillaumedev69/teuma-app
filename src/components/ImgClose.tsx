a4143d61db9a38a8a3f74d384cce4732a9c19a4b
Guillaume Destree <devstree.guillaume@gmail.com>
2024-09-25 23:25:32 +0200

    modification branch

--- src/components/ImgClose.tsx
+++ src/components/ImgClose.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import IconClose from "../images/xmark-solid.svg";
-import styles from "@/styles/Global.module.scss"
+import styles from "@/Global.module.scss"
 const ImgClose = () => {
     return (
         <div className={styles.iconClose}>
