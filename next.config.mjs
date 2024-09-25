c60ebeb3cf9cba753dcd5a3f0d3ff97d1756e230
Guillaume Destree <devstree.guillaume@gmail.com>
2024-09-25 23:37:51 +0200

    modification config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Remplace __dirname avec ce qui suit
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
 export default nextConfig;
