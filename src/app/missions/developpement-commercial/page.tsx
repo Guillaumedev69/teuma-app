a668b2490cee0ede2a87a3dca9b2323cd854c5fd
Guillaume Destree <devstree.guillaume@gmail.com>
2024-09-26 23:53:05 +0200

    correction bug dev commercial

--- src/app/missions/developpement-commercial/page.tsx
+++ src/app/missions/developpement-commercial/page.tsx
@@ -1,64 +1,63 @@
 import Cards from "@/components/Cards";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import styles from "@/styles/Developpement.module.scss"
+import styles from "@/styles/Developpement.module.scss";
 
-
-const page= () => {
+const page = () => {
     const donneesCard = [
         {
             id: 1,
             title: `Élaboration d’une stratégie commerciale`,
             description: `
-
-Le recrutement est le premier axe RH stratégique.
-
- Identifier les bons profils, diffuser des annonces attractives, sourcer les candidats, les rencontrer, et faire les bons choix.
-
-Je peux également intervenir dans l’intégration, la mise en place d’un onboarding et d’un plan de formation, pour  limiter le turn-over.
-
-`,
+                Le recrutement est le premier axe RH stratégique.
+                Identifier les bons profils, diffuser des annonces attractives, sourcer les candidats, les rencontrer, et faire les bons choix.
+                Je peux également intervenir dans l’intégration, la mise en place d’un onboarding et d’un plan de formation, pour  limiter le turn-over.
+            `,
         },
         {
             id: 2,
             title: `Administration du personnel.  
-Etablissement des contrats de travail et des DUE.
-Suivi des éléments variables de paie.`,
-            description: `Je m’occupe pour vous du rassemblement des différents éléments variables de paie. 
-
-Primes, absences, arrêts de travail.
-
-Pour mise en relation avec le cabinet d’expertise comptable en charge des paies, ou pour établissement des bulletins de paie. `,
+            Etablissement des contrats de travail et des DUE.
+            Suivi des éléments variables de paie.`,
+            description: `
+                Je m’occupe pour vous du rassemblement des différents éléments variables de paie. 
+                Primes, absences, arrêts de travail.
+                Pour mise en relation avec le cabinet d’expertise comptable en charge des paies, ou pour établissement des bulletins de paie.
+            `,
         },
         {
             id: 3,
             title: `Droit social : analyse de cas en situation.
- Appui juridique et opérationnel aux dirigeants. 
-Aide à la gestion du disciplinaire`,
-            description: `Juriste en droit social et manager opérationnel aguerri, j’allie mes connaissances juridiques et mon expérience professionnelle au service de votre entreprise. 
-
-Mon vécu me permet de vous accompagner, vous et vos managers, dans le quotidien de vos relations individuelles de travail.`,
+             Appui juridique et opérationnel aux dirigeants. 
+            Aide à la gestion du disciplinaire`,
+            description: `
+                Juriste en droit social et manager opérationnel aguerri, j’allie mes connaissances juridiques et mon expérience professionnelle au service de votre entreprise.
+                Mon vécu me permet de vous accompagner, vous et vos managers, dans le quotidien de vos relations individuelles de travail.
+            `,
         },
         {
             id: 4,
             title: `Analyse des KPI pour développer votre rentabilité: CA, marges, frais ...`,
-            description: `J’interviens comme manager externalisé auprès de vos collaborateurs et de votre force de vente.
-
-Pour des moments d’écoute, d’échanges et d’accompagnement. Pour fixer des objectifs et réaliser les débriefings d’activité nécessaire.
-
- Si cela vous permet de vous consacrer à la gestion de votre société.
-
-`,
+            description: `
+                J’interviens comme manager externalisé auprès de vos collaborateurs et de votre force de vente.
+                Pour des moments d’écoute, d’échanges et d’accompagnement. Pour fixer des objectifs et réaliser les débriefings d’activité nécessaire.
+                Si cela vous permet de vous consacrer à la gestion de votre société.
+            `,
         },
+    ];
 
-    ]
     return (
         <div>
             <Header />
             <h1>Développement commercial</h1>
             <div className={styles.cardsGrid}>
-                {donneesCard.map((card) => (
-                    <Cards key={card.id} title={card.title} description={card.description} />
+                {donneesCard.map((card, index) => (
+                    <Cards
+                        key={card.id}
+                        index={index}
+                        title={card.title}
+                        description={card.description}
+                    />
                 ))}
             </div>
             <Footer />
