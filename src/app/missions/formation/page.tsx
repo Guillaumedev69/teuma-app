c16adcfdf72bb95d8cfd98030ae69a6b490b8ebd
Guillaume Destree <devstree.guillaume@gmail.com>
2024-09-26 23:54:39 +0200

    correction bug formation

--- src/app/missions/formation/page.tsx
+++ src/app/missions/formation/page.tsx
@@ -37,8 +37,13 @@ const page = () => {
             <Header />
             <h1>Formation</h1>
             <div className={styles.cardsGrid}>
-                {donneesCard.map((card) => (
-                    <Cards key={card.id} title={card.title} description={card.description} />
+                {donneesCard.map((card, index) => (
+                    <Cards
+                        key={card.id}
+                        index={index}
+                        title={card.title}
+                        description={card.description}
+                    />
                 ))}
             </div>
             <Footer />
