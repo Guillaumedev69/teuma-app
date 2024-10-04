287aae14052ae1a366caf0658023723f59e9f65c
Guillaume Destree <devstree.guillaume@gmail.com>
2024-10-05 00:52:25 +0200

    changement de couleur hexagone

--- src/components/Hexagon.tsx
+++ src/components/Hexagon.tsx
@@ -6,14 +6,14 @@ import Polygon2 from "@/images/Polygon-2.webp"
 import Polygon2bis from "@/images/Polygon-2bis.webp"
 import Polygon3 from "@/images/Polygon-3.webp"
 import Polygon3bis from "@/images/Polygon-3bis.webp"
-import PolygonMini1 from "@/images/PolygonMini1.webp"
-import PolygonMini2 from "@/images/PolygonMini2.webp"
+/*import PolygonMini1 from "@/images/PolygonMini1.webp"*/
+/*import PolygonMini2 from "@/images/PolygonMini2.webp"*/
 import PolygonMini3 from "@/images/PolygonMini3.webp"
-import PolygonMini4 from "@/images/PolygonMini4.webp"
-import PolygonMini5 from "@/images/PolygonMini5.webp"
+/*import PolygonMini4 from "@/images/PolygonMini4.webp"*/
+/*import PolygonMini5 from "@/images/PolygonMini5.webp"*/
 import PolygonMini6 from "@/images/PolygonMini6.webp"
 import PolygonMini7 from "@/images/PolygonMini7.webp"
-import PolygonMini8 from "@/images/PolygonMini8.webp"
+/*import PolygonMini8 from "@/images/PolygonMini8.webp"*/
 import PolygonMini9 from "@/images/PolygonMini9.webp"
 
 const Hexagon = () => {
@@ -21,11 +21,11 @@ const Hexagon = () => {
         <section className={styles.sectionHexagone}>
             <Image src={Polygon1} alt="hexagone" className={styles.polygon1} />
             <div className={styles.line1}>
-                <Image src={PolygonMini1} alt="hexagone" className={styles.polygonMini1} />
-                <Image src={PolygonMini1} alt="hexagone" className={styles.polygonMini1bis} />
+                <div className={styles.polygonMini1}></div>
+                <div className={styles.polygonMini1bis}></div>
             </div>
             <div className={styles.line2}>
-                <Image src={PolygonMini2} alt="hexagone" className={styles.polygonMini2} />
+                <div className={styles.polygonMini2}></div>
                 <div className={styles.hexagoneFirst}>
                     <p className={styles.hexagoneFirstP}>À la recherche de solutions ponctuelles ou durables
                         pour gérer et développer votre entreprise ?
@@ -35,9 +35,9 @@ const Hexagon = () => {
             </div>
             <div className={styles.line3}>
                 <Image src={Polygon1bis} alt="hexagone" className={styles.polygon1bis1} />
-                <Image src={PolygonMini2} alt="hexagone" className={styles.polygonMini2bis} />
-                <Image src={PolygonMini4} alt="hexagone" className={styles.polygonMini4} />
-                <Image src={PolygonMini5} alt="hexagone" className={styles.polygonMini5} />
+                <div className={styles.polygonMini2bis}></div>
+                <div className={styles.polygonMini4}></div>
+                <div className={styles.polygonMini5}></div>
             </div>
             <div className={styles.line4}>
                 <Image src={Polygon2} alt="hexagone" className={styles.polygon2} />
@@ -54,14 +54,15 @@ const Hexagon = () => {
                 <Image src={PolygonMini6} alt="hexagone" className={styles.polygonMini6bis} />
             </div>
             <div className={styles.line5}>
-                <Image src={PolygonMini5} alt="hexagone" className={styles.polygonMini5bis} />
+                <div className={styles.polygonMini5bis}></div>
                 <Image src={Polygon3bis} alt="hexagone" className={styles.polygon3ter} />
                 <Image src={PolygonMini7} alt="hexagone" className={styles.polygonMini7bis} />
                 <Image src={Polygon3} alt="hexagone" className={styles.polygon3} />
                 <Image src={Polygon3bis} alt="hexagone" className={styles.polygon3bis} />
                 <Image src={PolygonMini9} alt="hexagone" className={styles.polygonMini9} />
-                <Image src={PolygonMini9} alt="hexagone" className={styles.polygonMini9bis} />
+                <div className={styles.polygonMini9bis}></div>
                 <Image src={PolygonMini7} alt="hexagone" className={styles.polygonMini7} />
+                
             </div>
             <div className={styles.line6}>
                 <div className={styles.hexagoneThird}>
@@ -71,8 +72,8 @@ const Hexagon = () => {
                         des TPE et PME
                     </p>
                 </div>
-                <Image src={PolygonMini8} alt="hexagone" className={styles.polygonMini8} />
-                <Image src={PolygonMini9} alt="hexagone" className={styles.polygonMini9ter} />
+                <div className={styles.polygonMini8}></div>
+                <div className={styles.polygonMini9ter}></div>
             </div>
         </section>
 
