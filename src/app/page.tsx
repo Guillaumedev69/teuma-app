5c40fe94c2761ef90411717db402307227b11e91
Guillaume Destree <devstree.guillaume@gmail.com>
2024-10-13 02:46:55 +0200

    nouvelle homePage manque ecran mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Footer from "@/components/Footer"
 import Header from "@/components/Header"
 import Hexagon from "@/components/Hexagon"
 import ImgCyrille from "@/components/ImgCyrille"
+import ImgCyrille2 from "@/components/ImgCyrille2"
 import ReviewGoogle from "@/components/ReviewGoogle"
 
 import styles from "@/styles/Home.module.scss"
@@ -15,7 +16,6 @@ export default function Home() {
         <div className={styles.homePage}>
             <Header />
             <main className={styles.mainHomePage} >
-
                 <section className={styles.sectionIntro}>
                     <div className={styles.h1BtnContain}>
                         <h1 className={styles.titleh1}>
@@ -31,6 +31,18 @@ export default function Home() {
                 <section className={styles.sectionReview}>
                     <h2>Vos avis sont importants</h2>
                     <ReviewGoogle />
+                </section>
+                <section className={styles.sectionAbout}>
+                    <h1>À propos de Teuma SME</h1>
+                    <ImgCyrille2 />
+                    <h2>Cyrille TEUMA - Dirigeant</h2>
+                    <p>Diplômé d&apos;un Master en droit social et fort de 15 ans d&apos;expérience dans le management d&apos;équipes
+                        et la gestion de centres de profits, j&apos;interviens à temps partagé et vous fais bénéficier aussi bien d
+                        e mes compétences en relations humaines et commerciales qu&apos;en gestion financière.<br /><br />
+                        Commerçant, pédagogue et disponible, je mettrai toute mon expertise pour mener à bien mes missions et
+                        vous aider à atteindre vos objectifs.<br /><br />
+                        Ancien Directeur de magasin en distribution spécialisée, et capable d&apos;allier performance économique et développement humain, je saurai vous proposer un accompagnement selon vos besoin.
+                    </p>
                 </section>
             </main>
             <Footer />
--- src/components/Hexagon.tsx
+++ src/components/Hexagon.tsx
@@ -6,74 +6,71 @@ import Polygon2 from "@/images/Polygon-2.webp"
 import Polygon2bis from "@/images/Polygon-2bis.webp"
 import Polygon3 from "@/images/Polygon-3.webp"
 import Polygon3bis from "@/images/Polygon-3bis.webp"
-/*import PolygonMini1 from "@/images/PolygonMini1.webp"*/
-/*import PolygonMini2 from "@/images/PolygonMini2.webp"*/
+import PolygonMini1 from "@/images/PolygonMini1.webp"
+import PolygonMini2 from "@/images/PolygonMini2.webp"
 import PolygonMini3 from "@/images/PolygonMini3.webp"
-/*import PolygonMini4 from "@/images/PolygonMini4.webp"*/
-/*import PolygonMini5 from "@/images/PolygonMini5.webp"*/
+import PolygonMini4 from "@/images/PolygonMini4.webp"
+import PolygonMini5 from "@/images/PolygonMini5.webp"
 import PolygonMini6 from "@/images/PolygonMini6.webp"
 import PolygonMini7 from "@/images/PolygonMini7.webp"
-/*import PolygonMini8 from "@/images/PolygonMini8.webp"*/
-/*import PolygonMini9 from "@/images/PolygonMini9.webp"*/
+import PolygonMini8 from "@/images/PolygonMini8.webp"
+import PolygonMini9 from "@/images/PolygonMini9.webp"
 
 const Hexagon = () => {
     return (
-        <section className={styles.sectionHexagone}>
-            <Image src={Polygon1} alt="hexagone" className={styles.polygon1} />
-            <div className={styles.line1}>
-                <div className={styles.polygonMini1}></div>
-                <div className={styles.polygonMini1bis}></div>
+        <section className={styles.sectionHexagon}>
+            <div className={styles.sectionHexagon__background}>
+                <div className={styles.backgroundLine1}>
+                    <Image src={Polygon1} alt="hexagone" className={styles.polygon1} />
+                    <Image src={PolygonMini1} alt="hexagone" className={styles.polygonMini1} />
+                    <Image src={PolygonMini1} alt="hexagone" className={styles.polygonMini1bis} />
+                    <Image src={PolygonMini2} alt="hexagone" className={styles.polygonMini2} />
+                    <Image src={PolygonMini3} alt="hexagone" className={styles.polygonMini3} />
+                    <Image src={Polygon1bis} alt="hexagone" className={styles.polygon1bis1} />
+                    <Image src={PolygonMini2} alt="hexagone" className={styles.polygonMini2bis} />
+                </div>
+                <div className={styles.backgroundLine2}>
+                    <Image src={PolygonMini4} alt="hexagone" className={styles.polygonMini4} />
+                    <Image src={PolygonMini5} alt="hexagone" className={styles.polygonMini5} />
+                    <Image src={Polygon2} alt="hexagone" className={styles.polygon2} />
+                    <Image src={Polygon2bis} alt="hexagone" className={styles.polygon2bis} />
+                    <Image src={PolygonMini6} alt="hexagone" className={styles.polygonMini6} />
+                    <Image src={PolygonMini6} alt="hexagone" className={styles.polygonMini6bis} />
+                    <Image src={PolygonMini5} alt="hexagone" className={styles.polygonMini5bis} />
+                </div>
+                <div className={styles.backgroundLine3}>
+                    <Image src={Polygon3bis} alt="hexagone" className={styles.polygon3ter} />
+                    <Image src={PolygonMini7} alt="hexagone" className={styles.polygonMini7bis} />
+                    <Image src={Polygon3} alt="hexagone" className={styles.polygon3} />
+                    <Image src={Polygon3bis} alt="hexagone" className={styles.polygon3bis} />
+                    <Image src={PolygonMini9} alt="hexagone" className={styles.polygonMini9} />
+                    <Image src={PolygonMini9} alt="hexagone" className={styles.polygonMini9bis} />
+                    <Image src={PolygonMini7} alt="hexagone" className={styles.polygonMini7} />
+                    <Image src={PolygonMini8} alt="hexagone" className={styles.polygonMini8} />
+                    <Image src={PolygonMini9} alt="hexagone" className={styles.polygonMini9ter} />
+                </div>
             </div>
-            <div className={styles.line2}>
-                <div className={styles.polygonMini2}></div>
-                <div className={styles.hexagoneFirst}>
-                    <p className={styles.hexagoneFirstP}>À la recherche de solutions ponctuelles ou durables
+            <div className={styles.sectionHexagon__content}>
+                <div className={styles.hexagonFirst}>
+                    <p className={styles.hexagonFirstP}>À la recherche de solutions ponctuelles ou durables
                         pour gérer et développer votre entreprise ?
                     </p>
                 </div>
-                <Image src={PolygonMini3} alt="hexagone" className={styles.polygonMini3} />
-            </div>
-            <div className={styles.line3}>
-                <Image src={Polygon1bis} alt="hexagone" className={styles.polygon1bis1} />
-                <div className={styles.polygonMini2bis}></div>
-                <div className={styles.polygonMini4}></div>
-                <div className={styles.polygonMini5}></div>
-            </div>
-            <div className={styles.line4}>
-                <Image src={Polygon2} alt="hexagone" className={styles.polygon2} />
-                <Image src={Polygon2bis} alt="hexagone" className={styles.polygon2bis} />
-                <Image src={PolygonMini6} alt="hexagone" className={styles.polygonMini6} />
-                <div className={styles.hexagoneSecond}>
-                    <p className={styles.hexagoneSecondP}>ACCOMPAGNEMENT
+                <div className={styles.hexagonSecond}>
+                    <p className={styles.hexagonSecondP}>ACCOMPAGNEMENT
                         DES DIRIGEANTS
                         dans leur stratégie
                         tactique et
                         operationnelle
                     </p>
                 </div>
-                <Image src={PolygonMini6} alt="hexagone" className={styles.polygonMini6bis} />
-            </div>
-            <div className={styles.line5}>
-                <div className={styles.polygonMini5bis}></div>
-                <Image src={Polygon3bis} alt="hexagone" className={styles.polygon3ter} />
-                <Image src={PolygonMini7} alt="hexagone" className={styles.polygonMini7bis} />
-                <Image src={Polygon3} alt="hexagone" className={styles.polygon3} />
-                <Image src={Polygon3bis} alt="hexagone" className={styles.polygon3bis} />
-                <div className={styles.polygonMini9}></div>
-                <div className={styles.polygonMini9bis}></div>
-                <Image src={PolygonMini7} alt="hexagone" className={styles.polygonMini7} />
-                
-            </div>
-            <div className={styles.line6}>
-                <div className={styles.hexagoneThird}>
-                    <p className={styles.hexagoneThirdP}>CADRE
+                <div className={styles.hexagonThird}>
+                    <p className={styles.hexagonThirdP}>CADRE
                         EXTERNALISÉ
                         au servide
                         des TPE et PME
                     </p>
                 </div>
-                <div className={styles.polygonMini8}></div>
-                <div className={styles.polygonMini9ter}></div>
             </div>
         </section>
 
